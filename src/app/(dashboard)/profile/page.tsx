3a1418f26dbd5247f9d59f137b5845ade12e99a8
MridulSheikh <mridul.sheikh.90@gmail.com>
2025-11-11 01:36:45 +0600

    new changes

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,24 +1,32 @@
+'use client'
 import { UserAvatar } from '@/components/shared/UserAvatar'
 import { Button } from '@/components/ui/button'
 import { UpdateProfile } from '@/components/ui/profile/UpdateProfileModal'
-import { Key, Pencil } from 'lucide-react'
+import { useCurrentUser } from '@/redux/features/auth/authSlice'
+import { useAppSelector } from '@/redux/hooks'
+import { Key, Mail } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Profile = () => {
+  const user = useAppSelector(useCurrentUser);
   return (
+    <div>
       <div className='w-full flex justify-between items-center'>
           <div className=' flex gap-x-5 items-center'>
               <UserAvatar userName='mridul sheikh' className=' size-20' />
               <div>
-                <h1 className=' text-2xl'>Mridul Sheikh</h1>
-                <p className=' mt-0.5 text-gray-600'>prince9nazir@gmail.com</p>
+                <h1 className=' text-2xl'>{user?.name}</h1>
+                <p className=' mt-0.5 text-gray-600'>{user?.email}</p>
               </div>
           </div>
           <div className=' flex gap-x-2'>
                <UpdateProfile />
+               <Button variant={"outline"}> <Mail/> Change email</Button>
                <Button> <Key/> Change Password</Button>
           </div>
       </div>
+       </div>
   )
 }
 
--- src/components/ui/profile/UpdateProfileModal.tsx
+++ src/components/ui/profile/UpdateProfileModal.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -11,9 +12,12 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useCurrentUser } from "@/redux/features/auth/authSlice"
+import { useAppSelector } from "@/redux/hooks"
 import { Pencil } from "lucide-react"
 
 export function UpdateProfile() {
+    const user = useAppSelector(useCurrentUser);
   return (
     <Dialog>
       <form>
@@ -31,11 +35,7 @@ export function UpdateProfile() {
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
+              <Input id="name-1" name="name" defaultValue={user?.name} />
             </div>
           </div>
           <DialogFooter>
